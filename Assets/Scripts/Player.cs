df0268e2ebe420c56d6b87edff089c0f584f1242
Anthony <anthonylegourrierec@gmail.com>
2023-04-09 19:07:13 +0200

    Gravity and jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,6 +9,14 @@ public class Player : MonoBehaviour
     private CharacterController myController;
     public Transform myCameraEyes;
     private float cameraVerticalRotation;
+    // Gravity
+    private Vector3 verticalVelocity;
+    public float gravity = 9.8f;
+    // Jump
+    public Transform ground;
+    public float jumpHeight = 0.02f, groundDistance = 10.0f;
+    public LayerMask groundLayer;
+    private bool readyToJump;
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
@@ -21,11 +29,12 @@ public class Player : MonoBehaviour
     {
         PlayerMovement();
         CameraMovement();
-
+        Jump();
     }
 
     private void PlayerMovement()
     {
+        //Move the player on the xz plane
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
@@ -33,10 +42,14 @@ public class Player : MonoBehaviour
 
         movement = movement * speed * Time.deltaTime;
         myController.Move(movement);
+        //Add gravity influence on the player
+        verticalVelocity.y += 0.5f * Physics.gravity.y * Mathf.Pow(Time.deltaTime, 2) * gravity;
+        myController.Move(verticalVelocity);
     }
 
     private void CameraMovement()
     {
+        //Rotate the camera along X and Y axes
         float mouseX = Input.GetAxisRaw("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxisRaw("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
@@ -45,5 +58,21 @@ public class Player : MonoBehaviour
 
         transform.Rotate(Vector3.up * mouseX);
         myCameraEyes.localRotation = Quaternion.Euler(cameraVerticalRotation, 0f, 0f);
+    }
+
+    private void Jump()
+    {
+        RaycastHit hit;
+        bool isGrounded = Physics.Raycast(transform.position, Vector3.down, out hit, groundDistance, groundLayer);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            Debug.Log("Jump");
+            verticalVelocity.y = Mathf.Sqrt(-2 * jumpHeight * Physics.gravity.y);
+        }
+
+        // Add gravity influence on the player
+        verticalVelocity.y += 0.5f * Physics.gravity.y * Mathf.Pow(Time.deltaTime, 2) * gravity;
+        myController.Move(verticalVelocity * Time.deltaTime);
     }
 }
