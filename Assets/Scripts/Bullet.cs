7c397d6b1aba6febb8280acd28671f1e69be9d5c
Anthony <anthonylegourrierec@gmail.com>
2023-04-13 17:54:40 +0200

    Basic player shoot

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 100f, bulletLife = 2f;
+    public Rigidbody myRigidBody;
+
+    void Awake()
+    {
+        myRigidBody = GetComponent<Rigidbody>();
+    }
+    void Start()
+    {
+        Destroy(gameObject, bulletLife);
+    }
+
+
+    private void FixedUpdate()
+    {
+        myRigidBody.MovePosition(transform.position + transform.forward * speed * Time.fixedDeltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
+    /* private IEnumerator DestroyBulletTime()
+     {
+         yield return new WaitForSeconds(bulletLife);
+         Destroy(gameObject);
+     }*/
+
+    private void DestroyBulletTime()
+    {
+        bulletLife -= Time.deltaTime;
+        if (bulletLife < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/GunSystem.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunSystem : MonoBehaviour
+{
+    public GameObject projectile;
+    public Transform firingPosition;
+    public Transform myCameraEyes;
+    public float timeBetweenShot = 0.2f, gunDamage = 1f;
+    private bool readyToShoot = true;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        Shoot();
+    }
+
+    private void Shoot()
+    {
+        if (Input.GetMouseButton(0) && readyToShoot)
+        {
+            readyToShoot = false;
+            RaycastHit hit;
+            if (Physics.Raycast(myCameraEyes.position, myCameraEyes.forward, out hit, 100f))
+            {
+                if (hit.collider.tag.Equals("Enemy"))
+                {
+                    hit.collider.GetComponent<EnemyHealthSystem>().TakeDamage(gunDamage);
+                }
+                firingPosition.LookAt(hit.point);
+            }
+            else
+            {
+                Vector3 targetPosition = myCameraEyes.position + myCameraEyes.forward * 50f;
+                firingPosition.LookAt(targetPosition);
+            }
+            Instantiate(projectile, firingPosition.position, firingPosition.rotation);
+            StartCoroutine(ResetShot());
+        }
+    }
+
+    IEnumerator ResetShot()
+    {
+        yield return new WaitForSeconds(timeBetweenShot);
+        readyToShoot = true;
+    }
+}
